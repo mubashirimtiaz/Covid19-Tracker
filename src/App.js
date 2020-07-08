57b11a3e687dbd3af2c457e8b1ea6adc951604c7
Mubashir Imtiaz <mubashir.imtiaz123@gmail.com>
2020-07-09 01:01:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="display-3">Covid19-tracker</h1>
+      <h1 className="display-3">Covid19-tracker!!!</h1>
     </div>
   );
 }
